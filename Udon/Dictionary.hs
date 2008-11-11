3e5516829d5e73ec2f9f5875b195c26b33baf1ab
Luke Palmer <lrpalmer@gmail.com>
2008-11-11 02:03:14 -0700

    Made a Dictionary type.

---
+++ Udon/Dictionary.hs
@@ -0,0 +1,22 @@
+module Udon.Dictionary 
+    ( Dictionary
+    , getDictionaryMap, makeDictionary
+    )
+where
+
+import Data.Binary
+import Udon.Types
+import Udon.TypedBlob
+import qualified Data.Map as Map
+
+newtype Dictionary = Dictionary (Map.Map String TypedBlob)
+    deriving Binary
+
+getDictionaryMap :: Dictionary -> Map.Map String TypedBlob
+getDictionaryMap (Dictionary m) = m
+
+makeDictionary :: Map.Map String TypedBlob -> Dictionary
+makeDictionary = Dictionary
+
+instance Typed Dictionary where
+    getType _ = TyContent ContentDictionary
