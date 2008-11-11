ce8e8047b4b6d22f9e9672183d59c74e6a600da0
Luke Palmer <lrpalmer@gmail.com>
2008-11-10 21:08:01 -0700

    Added some actual enapsulation to HashFS.  Added the HashFS monad, which may eventually handle locking/transactions in a repository (it's just as stupid as ever, but we are now allowed to make it smarter)

--- Udon/HashFS.hs
+++ Udon/HashFS.hs
@@ -1,4 +1,13 @@
-module Udon.HashFS where
+module Udon.HashFS 
+    ( Repo, Ref
+    , HashFS
+    , makeRepo, openRepo, closeRepo
+    , inRepo
+    , fromStr
+    , addBlob, getBlob
+    , addObject, getObject
+    )
+where
 
 import qualified Data.Digest.SHA2 as SHA2
 import System.IO
@@ -8,9 +17,13 @@ import qualified Data.ByteString.Lazy.Char8 as Char8
 import qualified Codec.Binary.Base64.String as Base64
 import Control.Applicative
 import Data.Binary
+import Control.Monad.Reader
 
 newtype Repo = Repo FilePath
 newtype Ref = Ref Str.ByteString
+
+newtype HashFS a = HashFS (ReaderT Repo IO a)
+    deriving (Functor, Monad)
 
 instance Binary Ref where
     get = Ref <$> get
@@ -18,9 +31,6 @@ instance Binary Ref where
 
 instance Show Ref where
     show r = "fromStr \"" ++ asciiHash r ++ "\""
-
--- This should really all be in a filesystem transaction monad.
--- It is horribly unsafe at the moment.
 
 makeRepo :: FilePath -> IO Repo
 makeRepo path = do
@@ -38,6 +48,9 @@ openRepo path = do
 closeRepo :: Repo -> IO ()
 closeRepo _ = return ()
 
+inRepo :: Repo -> HashFS a -> IO a
+inRepo repo (HashFS m) = runReaderT m repo
+
 asciiHash :: Ref -> String
 asciiHash (Ref bs) = map subst . filter (/= '\n') . Base64.encode . Char8.unpack $ bs
     where
@@ -52,22 +65,24 @@ fromStr = Ref . Char8.pack . Base64.decode . map subst
 
 sha256 = Ref . Str.pack . SHA2.toOctets . SHA2.sha256 . Str.unpack
 
-addBlob :: Repo -> Str.ByteString -> IO Ref
-addBlob (Repo path) blob = do
+addBlob :: Str.ByteString -> HashFS Ref
+addBlob blob = HashFS $ do
+    Repo path <- ask
     let hash = sha256 blob
-    Str.writeFile (path ++ "/objects/" ++ asciiHash hash) blob
+    liftIO $ Str.writeFile (path ++ "/objects/" ++ asciiHash hash) blob
     return hash
 
-getBlob :: Repo -> Ref -> IO (Maybe Str.ByteString)
-getBlob (Repo path) hash = do
+getBlob :: Ref -> HashFS (Maybe Str.ByteString)
+getBlob hash = HashFS $ do
+    Repo path <- ask
     let file = path ++ "/objects/" ++ asciiHash hash
-    exists <- doesFileExist file
+    exists <- liftIO $ doesFileExist file
     if exists
-        then Just <$> Str.readFile file
+        then Just <$> liftIO (Str.readFile file)
         else return Nothing
 
-addObject :: (Binary b) => Repo -> b -> IO Ref
-addObject repo = addBlob repo . encode
+addObject :: (Binary b) => b -> HashFS Ref
+addObject = addBlob . encode
 
-getObject :: (Binary b) => Repo -> Ref -> IO (Maybe b)
-getObject repo ref = fmap decode <$> getBlob repo ref
+getObject :: (Binary b) => Ref -> HashFS (Maybe b)
+getObject ref = fmap decode <$> getBlob ref
