8679a21c679650ffc0420806c9ceb57c6ce65181
Luke Palmer <lrpalmer@gmail.com>
2008-11-11 01:59:31 -0700

    Added a Text type.

---
+++ Udon/Text.hs
@@ -0,0 +1,19 @@
+module Udon.Text 
+    ( Text, makeText, getText )
+where
+
+import qualified Data.ByteString.Lazy as Str
+import Data.Binary
+import Udon.Types
+
+newtype Text = Text Str.ByteString
+    deriving Binary
+
+makeText :: Str.ByteString -> Text
+makeText = Text
+
+getText :: Text -> Str.ByteString
+getText (Text s) = s
+
+instance Typed Text where
+    getType _ = TyContent ContentText
