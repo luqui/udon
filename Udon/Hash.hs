2b838d1d09985937eab0eb39a777c7e361910ea2
Luke Palmer <lrpalmer@gmail.com>
2008-11-12 03:09:29 -0700

    Added export list so it's actually abstract.

--- Udon/Hash.hs
+++ Udon/Hash.hs
@@ -1,16 +1,20 @@
-module Udon.Hash where
+module Udon.Hash 
+    ( Blob, Hash, hashBlob ) 
+where
 
 import qualified Data.ByteString.Lazy as Str
 import qualified Data.Digest.SHA256 as SHA
 import Data.Binary
 import Control.Applicative
 
-newtype Hash = Hash Str.ByteString
+type Blob = Str.ByteString
+
+newtype Hash = Hash Blob
     deriving Eq
 
 instance Binary Hash where
     put (Hash x) = put x
     get = Hash <$> get
 
-hashBlob :: Str.ByteString -> Hash
+hashBlob :: Blob -> Hash
 hashBlob = Hash . Str.pack . SHA.hash . Str.unpack
